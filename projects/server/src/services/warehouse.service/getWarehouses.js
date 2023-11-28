460af74e17d9d21858b2f3345867f0eeae1797c6
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-11-28 16:57:00 +0700

    feat(Warehouse): add pagination and update filtering

--- projects/server/src/services/warehouse.service/getWarehouses.js
+++ projects/server/src/services/warehouse.service/getWarehouses.js
@@ -1,7 +1,36 @@
-const { Warehouse, WarehouseAddress, Province, City } = require('../../models');
+const {
+  Sequelize,
+  Warehouse,
+  WarehouseAddress,
+  Province,
+  City,
+} = require('../../models');
 
-async function getWarehouses() {
-  const warehouses = await Warehouse.findAll({
+function generateFilters(req) {
+  // set default values
+  if (typeof req.query.pagination !== 'boolean') req.query.pagination = false;
+  req.query.sortBy = req.query.sortBy || 'updatedAt';
+  req.query.orderBy = req.query.orderBy || 'DESC';
+  req.query.page = req.query.page || 1;
+  req.query.perPage = req.query.perPage || 10;
+
+  const { search, sortBy, orderBy, pagination, page, perPage } = req.query;
+  return {
+    logging: false,
+    where: {
+      [Sequelize.Op.or]: {
+        name: { [Sequelize.Op.like]: `%${search || ''}%` },
+        '$WarehouseAddress.Province.name$': {
+          [Sequelize.Op.like]: `%${search || ''}%`,
+        },
+        '$WarehouseAddress.City.name$': {
+          [Sequelize.Op.like]: `%${search || ''}%`,
+        },
+      },
+    },
+    order: [[sortBy, orderBy]],
+    limit: pagination ? perPage : undefined,
+    offset: pagination ? (page - 1) * perPage : undefined,
     paranoid: false,
     include: [
       {
@@ -10,10 +39,31 @@ async function getWarehouses() {
         paranoid: false,
       },
     ],
-    order: [['updatedAt', 'DESC']],
-    logging: false,
-  });
-  return warehouses;
+  };
+}
+
+async function receiveWarehouses(req, filters) {
+  const totalData = (
+    await Warehouse.findAll({ ...filters, limit: undefined, offset: undefined })
+  ).length;
+  const categories = await Warehouse.findAll({ ...filters });
+  return [categories, totalData];
+}
+
+async function getWarehouses(req) {
+  const filters = generateFilters(req);
+  const [categories, totalData] = await receiveWarehouses(req, filters);
+
+  const { pagination, page, perPage } = req.query;
+  const paginationInfo = { totalData };
+  if (pagination) {
+    paginationInfo.currentPage = page;
+    paginationInfo.perPage = perPage;
+    paginationInfo.totalPage = Math.ceil(paginationInfo.totalData / perPage);
+    paginationInfo.offset = filters.offset;
+  }
+
+  return [categories, paginationInfo];
 }
 
 module.exports = getWarehouses;
