492dfc933428bc318976e7dc0cc2e9775d1e4585
ridhozhr10 <azharridho42@gmail.com>
2023-11-30 13:33:54 +0700

    ci: fix config env prod

--- deploy.config.js
+++ deploy.config.js
@@ -1,11 +1,29 @@
 module.exports = {
   apps: [
     {
-      name: 'JCWD-0306-01', // Format JCWD-{batchcode}-{groupnumber}
-      script: './projects/server/src/index.js',
+      name: "JCWD-0306-01", // Format JCWD-{batchcode}-{groupnumber}
+      script: "./projects/server/src/index.js",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         PORT: 8001,
+
+        // config database
+        MYSQL_USER: "jcwd030601",
+        MYSQL_PASSWORD: "jcwd030601",
+        MYSQL_DATABASE: "jcwd030601",
+        MYSQL_HOST: "adminer2.purwadhikabootcamp.com",
+        MYSQL_DIALECT: "mysql",
+        MYSQL_TIMEZONE: "+07:00",
+
+        JWT_SECRET_KEY: "RAHASIA",
+        nodemailer_email: "gadgetgallerygroup2@gmail.com",
+        nodemailer_password: "ngjodqocydilwihr",
+
+        OpenCage_api_key: "c79ccba183824510808d5899fc0f6e62",
+        RajaOngkir_api_key: "a44bb964bbfe84f56d5c38ca505def6e",
+        Googlemaps_api_key: "AIzaSyD0BznkOcWn2BonYVT94uhlkwz1DycFjxg",
+        URL: "https://jcwd030601.netlify.app/",
+        REDIS_URL: "redis://localhost:6379",
       },
       time: true,
     },
